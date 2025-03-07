e2dc17930f5130bd95838881cb6a5b3c3ae258d2
Her0x00 <hannes.heikkila07@gmail.com>
2025-03-07 09:00:49 +0200

    office image

--- FNAJ/src/scenes/office.ts
+++ FNAJ/src/scenes/office.ts
@@ -1,11 +1,18 @@
-import Phaser from "phaser";
-
 export class OfficeScene extends Phaser.Scene {
     constructor() {
         super({ key: "OfficeScene" });
     }
 
+    preload() {
+        this.load.image("officeBg", "assets/office.png"); 
+    }
+
     create() {
+        const {width, height } = this.scale
+        let officeBg = this.add.image(width / 2, height / 2, "officeBg")
+        .setOrigin(0.5, 0.5)
+        .setDisplaySize(width, height)
+
         this.add.text(100, 100, "Office View - Press C to Check Cameras", { fontSize: "20px", color: "#fff" });
 
         this.input.keyboard.once("keydown-C", () => {
