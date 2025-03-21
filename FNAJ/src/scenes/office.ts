7652ddc3f6661939d401fcc0d068b4d894cd2b62
Her0x00 <hannes.heikkila07@gmail.com>
2025-03-21 13:53:59 +0200

    Halv färdi

--- FNAJ/src/scenes/office.ts
+++ FNAJ/src/scenes/office.ts
@@ -7,6 +7,10 @@ export class OfficeScene extends Phaser.Scene {
     rightDoor!: Phaser.GameObjects.Rectangle; // Right door
     leftDoorClosed: boolean = false;  
     rightDoorClosed: boolean = false;  
+    leftLightOn: boolean = false
+    rightLightOn: boolean = false
+
+    officeBg!: Phaser.GameObjects.Image;  // Reference to the background
 
     constructor() {
         super({ key: "OfficeScene" });
@@ -16,7 +20,7 @@ export class OfficeScene extends Phaser.Scene {
         this.registry.set("GameState", new GameState());
         this.state = this.registry.get("GameState");
 
-        this.load.image("officeBg", "assets/office.png"); 
+        this.load.image("officeBg", "assets/office/1-OFFICE.png"); 
         //this.load.image("button", "assets/button.png"); // Load button image
     
         this.load.image("MonitorAnim1", "assets/monitorAnimation/1.png");
@@ -30,14 +34,19 @@ export class OfficeScene extends Phaser.Scene {
         this.load.image("MonitorAnim9", "assets/monitorAnimation/9.png");
         this.load.image("MonitorAnim10", "assets/monitorAnimation/10.png");
         this.load.image("MonitorAnim11", "assets/monitorAnimation/11.png"); 
-   
+
+        this.load.image("LeftDoor", "assets/office/DOORS/LEFT/14.png")
+        this.load.image("RightDoor", "assets/office/DOORS/RIGHT/14.png")
+        
+        this.load.image("OfficeLeftLight", "assets/office/LEFT_LIGHT.png")
+        this.load.image("OfficeRightLight", "assets/office/RIGHT_LIGHT.png")
     }
 
     create() {
         const { width, height } = this.scale;
 
-        // Adjust the width of the office scene (for example, make it 50% wider)
-        const newWidth = width * 1.2;
+        // lagar imaging brejadri men fan jävla 250p)
+        const newWidth = width * 1;
 
         // Background
         const officeBg = this.add.image(newWidth / 2, height / 2, "officeBg")
@@ -46,15 +55,12 @@ export class OfficeScene extends Phaser.Scene {
 
         this.add.text(100, 100, "Office View - Press C to Check Cameras", { fontSize: "20px", color: "#fff" });
 
-
-            //gör så att bakgrunding far fram å tibak som i dömde ader cams
-        this.tweens.add({
-            targets: officeBg,
-            x: { from: width / 2 - 120, to: width / 2 + 120 }, // far 100px från höge ti vänste
-            duration: 3500, // tidn 
-            ease: "Sine.easeInOut", // Smooth movement
-            yoyo: true, // far framåtibak
-            repeat: -1 // Loop forever
+        const lightButton = this.add.image(200, height - 100, "button")
+        .setInteractive()
+        .setScale(5);
+    
+        lightButton.on("pointerdown", () => {
+            this.toggleLights();
         });
 
         this.input.keyboard!.once("keydown-C", () => {
@@ -62,14 +68,17 @@ export class OfficeScene extends Phaser.Scene {
         });
 
         // höger Door
-        this.rightDoor = this.add.rectangle(this.scale.width - 250, 0, 100, this.scale.height, 0x888888)
+        this.rightDoor = this.add.image(this.scale.width - 250, 0, "RightDoor")
         .setOrigin(0.5, 0)
-        .setAlpha(0.8);
+        .setAlpha(0.8)
+        .setDisplaySize(250, this.scale.height)
 
         // vänster Door
-        this.leftDoor = this.add.rectangle(250, 0, 100, this.scale.height, 0x888888)
+        
+        this.leftDoor = this.add.image(200, 0, "LeftDoor")
             .setOrigin(0.5, 0)
-            .setAlpha(0.8);
+            .setAlpha(0.8)
+            .setDisplaySize(250, this.scale.height)
 
         // höger knapp
         const rightButton = this.add.image(width - 250, height - 100, "button")
@@ -87,7 +96,28 @@ export class OfficeScene extends Phaser.Scene {
         leftButton.on("pointerdown", () => {
             this.toggleDoor(this.leftDoor, "left");
         });
+
+        //lights
+        const leftLightButton = this.add.image(400, height - 50, "button")
+            .setInteractive()
+            .setScale(5);
+        
+        leftLightButton.on("pointerdown", () => {
+            this.toggleLeftLight();
+        });
+
+        // Button to toggle the right light on/off
+        const rightLightButton = this.add.image(width - 200, height - 100, "button")
+            .setInteractive()
+            .setScale(5);
+        
+        rightLightButton.on("pointerdown", () => {
+            this.toggleRightLight();
+        });
     }
+
+
+
     //kod för att öpin dörra höger ele vänster (((thank you gpt )))
     toggleDoor(door: Phaser.GameObjects.Rectangle, side: "left" | "right") {
         const isRight = side === "right";
@@ -106,7 +136,24 @@ export class OfficeScene extends Phaser.Scene {
             this.leftDoorClosed = !this.leftDoorClosed;
         }
     }
-    
+    toggleRightLight() {
+        this.rightLightOn = !this.rightLightOn;
+
+        if (this.leftLightOn && this.rightLightOn) {
+            // Both lights on
+            this.officeBg.setTexture("OfficeRightLight");
+        } else if (this.rightLightOn) {
+            // Only right light on
+            this.officeBg.setTexture("officeRightLight");
+        } else if (this.leftLightOn) {
+            // Only left light on
+            this.officeBg.setTexture("officeLeftLight");
+        } else {
+            // Both lights off
+            this.officeBg.setTexture("officeBg");
+        }
+    }
+
     
     
 
